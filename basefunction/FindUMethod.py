576daa6f20b726740f46377390470876f1473ed3
heesu <heesu@ajou.ac.kr>
2021-05-12 19:06:33 +0000

    Update auto_subtitle version

--- basefunction/FindUMethod.py
+++ basefunction/FindUMethod.py
@@ -3,50 +3,84 @@ import youtube_dl
 import os
 import glob
 from os.path import basename
+from konlpy.tag import Mecab
 
 YOUTUBE_REPO_PATH = '/home/heesu/mount/NLP/script'
 
-def MakeVttFile(URL):
-  downOption = {
+def MakeVttFile(URL,auto_sub=False):
+  DownOption = {
     'skip_download': True,
     'writesubtitles' : True,
-    'writeautomaticsub' : True ,
+    'writeautomaticsub' : auto_sub,
     'subtitleslangs':['ko'],
     'subtitlesformat': 'vtt',
     'nooverwrites':True,
     'outtmpl' : 'script/%(id)s'
   }
-  with youtube_dl.YoutubeDL(downOption) as ydl:
-      ydl.download([URL])
+  with youtube_dl.YoutubeDL(DownOption) as ydl:
+    ydl.download([URL])
+    ChkFile(URL)
+
+def ChkFile(URL):
+    if not os.path.exists(f'{YOUTUBE_REPO_PATH}/{ChkID(URL)}.ko.vtt'):
+        MakeVttFile(URL, auto_sub=True)
+        SubtitleFile = f'{YOUTUBE_REPO_PATH}/{ChkID(URL)}.ko.vtt'
+        f = open(SubtitleFile, 'r')
+        lines = f.readlines()
+        f.close()
+        fw = open(SubtitleFile, 'w')
+        for line in lines[:4]:
+            fw.write(line)
+        for idx, line in enumerate(lines[6::8]):
+            skip = False
+            time_stamp = True
+            clean = ''
+            time = ''
+            for c in line:
+                if c == '>':
+                    skip = False
+                    time_stamp = False
+                    continue
+
+                if c == '<' or skip:
+                    if time_stamp and skip:
+                        time += c
+                    skip = True
+                    continue
+
+                clean += c
+            if time_stamp:
+                try:
+                    time=lines[(idx+1)*8].split(' ')[0]
+                except:
+                    pass
+            fw.write(f'{time}\n{clean}\n')
+        fw.close()
 
 def ChkID(URL):
     id = URL.rsplit('/',1)[-1]
     return id
 
-def SelectFile(URL):
-    ID=ChkID(URL)
-    sub_file_list = glob.glob(os.path.join(YOUTUBE_REPO_PATH,'*.vtt'))
-    sub_file_list = map(lambda x:os.path.join(YOUTUBE_REPO_PATH,x),sub_file_list)
-
-    for sub_file in sub_file_list:
-        FileName = basename(sub_file)
-        SubtitleFile = FileName.split('.')[0]
-        if(ID==SubtitleFile):
-            return sub_file
-    MakeVttFile(URL)
-    return YOUTUBE_REPO_PATH + '/' + ID + '.ko.vtt'
-
 def Ctrl_F(keyword,URL):
-    SubtitleFile1 = SelectFile(URL)
+    SubtitleFile = f'{YOUTUBE_REPO_PATH}/{ChkID(URL)}.ko.vtt'
     TimeStamp=[]
-    with open(SubtitleFile1) as f:
+    with open(SubtitleFile) as f:
         lines = f.readlines()
-        StartPoint = 0
-        for line in lines:
+        StartPoint = 3
+        for line in lines[4:]:
             StartPoint += 1
             if keyword in line:
-                PlaySection = lines[StartPoint-2]
-                #print(line + PlaySection) 확인용
-                TimeStamp.append(PlaySection.split(' ')[0])
-    return TimeStamp 
-                
+                PlaySection = lines[StartPoint-1]
+                #print(line + PlaySection) #확인용
+                PlaySection = PlaySection.split(' ')[0]
+                TimeStamp.append(PlaySection.split('\n')[0])
+    #print(TimeStamp) #확인용
+    return TimeStamp  
+
+def OnlyNoun(URL):
+    with open(f'{YOUTUBE_REPO_PATH}/{ChkID(URL)}.ko.vtt','r', encoding='utf-8') as f:
+        script = f.read()
+    tagger = Mecab()
+    ex1 = tagger.nouns(script)
+    ex1
+    
