426bea3692194badaa09da9c6e66987412aeb617
rhcsky <dev.rhcsky@gmail.com>
2021-05-19 20:44:05 +0000

    Update FindU

--- FindU.py
+++ FindU.py
@@ -1,12 +1,17 @@
-#from STT import load_model, stt
+from STT import load_model, stt
+from pprint import pprint as pp
 from basefunction.FindUMethod import *
 
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
 if __name__ == "__main__":
-    i = input("fucntion num: 1(extract subtitle), 2(ctrl+F), 3(Frequency), 4(STT)")
+    i = input("fucntion num: 1(extract subtitle), 2(ctrl+F), 3(Frequency), 4(Check stt performance)")
 
     if i == '1':
         URL = input("URL:")
-        MakeVttFile(URL)
+        MakeFile(URL)
     if i == '2':
         SearchingValue = input("keyword:")
         URL = input("URL:")
@@ -15,12 +20,13 @@ if __name__ == "__main__":
         SearchingValue = input("keyword:")
         URL = input("URL:")
         Frequency(SearchingValue, URL)
-""" if i == '4':
-# no db ins
-print("Model loading... ")
-model, vocab = load_model()
-print("Done")
-audio_path = 'STT/audio/full.wav'
+    if i == '4':
+        # no db ins
+        print("Model loading... ")
+        model, vocab = load_model()
+        print("Done")
 
-sentences = stt(model, vocab, audio_path)
-pp(sentences)"""
+        audio_path = 'STT/data/audio/full.wav'
+
+        sentences = stt(model, vocab, audio_path)
+        pp(sentences)
