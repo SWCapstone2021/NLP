0c84ae153fcab4c308b200fbc9d7d36855098308
rhcsky <dev.rhcsky@gmail.com>
2021-06-03 11:46:25 +0000

    Implement sentence embedding model

--- FindU.py
+++ FindU.py
@@ -1,5 +1,6 @@
-import json
 import os
+
+from youtube_transcript_api import YouTubeTranscriptApi
 
 from QA import load_qa_model, QA_system
 from STT import load_stt_model, stt
@@ -11,31 +12,33 @@ from wordembedding import *
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
+
 def download_script(id):
-        transcript = YouTubeTranscriptApi.get_transcripts([id], languages=['ko'])
+    transcript = YouTubeTranscriptApi.get_transcripts([id], languages=['ko'])
 
-        transcript = transcript[0]
-        sub = transcript[id]
-        for x in sub:
-            x.pop('duration', None)
+    transcript = transcript[0]
+    sub = transcript[id]
+    for x in sub:
+        x.pop('duration', None)
 
-        return sub
+    return sub
+
 
 if __name__ == "__main__":
     i = input("fucntion num:  1(ctrl+F), 2(reliability), 3(STT), 4(association), 5(summarization), 6(QA)")
 
-    json_file=download_script('R_Llt7SnSFA')
+    json_file = download_script('R_Llt7SnSFA')
 
     if i == '1':
         SearchingValue = input("keyword:")
         result_script = ctrl_f(SearchingValue, json_file)
-        pp(result_script[:5])
+        # pp(result_script[:5])
 
     if i == '2':
         sc_model = load_sc_model()
         SearchingValue = input("keyword:")
         score = cosin_similar(SearchingValue, json_file, sc_model)
-        print(score)
+        # print(score)
 
     if i == '3':
         print("Load model...", end='')
@@ -46,12 +49,12 @@ if __name__ == "__main__":
 
         sentences = stt(stt_model, stt_vocab, audio_path)
         # pp(sentences[:5])
-    
+
     if i == '4':
         wm_model = load_wm_model()
         SearchingValue = input("keyword:")
         result_script = association_f(SearchingValue, json_file, wm_model)
-        pp(result_script)
+        # pp(result_script)
 
     if i == '5':
         summ_model = load_summ_model()
@@ -66,4 +69,3 @@ if __name__ == "__main__":
         question = '이혼한 날'
         answers = QA_system(qa_model, qa_tokenizer, question, json_file)
         # pp(answers[:5)
-    
--- Summarization/__init__.py
+++ Summarization/__init__.py
@@ -2,5 +2,5 @@ from pororo import Pororo
 
 
 def load_summ_model():
-    summ_model = Pororo(task="text_summarization", lang="ko", model="extractive")
-    return summ_model
+    model = Pororo(task="text_summarization", lang="ko", model="extractive")
+    return model
