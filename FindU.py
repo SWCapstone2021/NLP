09ec1157d2a06aaec14685257f1d23440a405bd6
HeesuNam <heesu@ajou.ac.kr>
2021-06-03 11:29:20 +0000

    Modify method for cos_sin

--- FindU.py
+++ FindU.py
@@ -4,29 +4,38 @@ import os
 from QA import load_qa_model, QA_system
 from STT import load_stt_model, stt
 from basefunction import ctrl_f
-from wordembedding import *
+from wordembedding import load_wm_model, cosin_similar,association_f, summary_script, load_summ_model, load_sc_model
 from pororo import Pororo
-
+from youtube_transcript_api import YouTubeTranscriptApi
 #from pprint import pprint as pp
+
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
-with open("test_scripts.json", "r") as st_json:
-    json_file = json.load(st_json)
-# script = json2list(json_file)
+def download_script(id):
+        transcript = YouTubeTranscriptApi.get_transcripts([id], languages=['ko'])
+
+        transcript = transcript[0]
+        sub = transcript[id]
+        for x in sub:
+            x.pop('duration', None)
+
+        return sub
 
 if __name__ == "__main__":
     i = input("fucntion num:  1(ctrl+F), 2(reliability), 3(STT), 4(association), 5(summarization), 6(QA)")
 
+    json_file=download_script('R_Llt7SnSFA')
+
     if i == '1':
         SearchingValue = input("keyword:")
         result_script = ctrl_f(SearchingValue, json_file)
-        # pp(result_script[:5])
+        pp(result_script[:5])
 
     if i == '2':
-        wm_model = load_wm_model()
+        sc_model = load_sc_model()
         SearchingValue = input("keyword:")
-        score = cosin_similar(SearchingValue, json_file, wm_model)
+        score = cosin_similar(SearchingValue, json_file, sc_model)
         print(score)
     
     if i == '3':
@@ -38,18 +47,18 @@ if __name__ == "__main__":
 
         sentences = stt(stt_model, stt_vocab, audio_path)
         # pp(sentences[:5])
-
+    
     if i == '4':
         wm_model = load_wm_model()
         SearchingValue = input("keyword:")
         result_script = association_f(SearchingValue, json_file, wm_model)
-        # pp(result_script[:5])
+        pp(result_script)
 
     if i == '5':
         summ_model = load_summ_model()
         summ_script = summary_script(json_file, summ_model)
-        #pp(summ_script)
-
+        pp(summ_script)
+    
     if i == '6':
         print("Load model...", end='')
         qa_model, qa_tokenizer = load_qa_model()
@@ -58,3 +67,4 @@ if __name__ == "__main__":
         question = '이혼한 날'
         answers = QA_system(qa_model, qa_tokenizer, question, json_file)
         # pp(answers[:5)
+    
